2e6eb6c054ed7d6e047defec9d78b2dddb8f7358
David Henry <david@decoybecoy.com>
2017-01-05 08:03:00 +0000

    Remove data element from historical migrations

--- db/migrate/20170104075044_add_role_to_user.rb
+++ db/migrate/20170104075044_add_role_to_user.rb
@@ -1,9 +1,5 @@
 class AddRoleToUser < ActiveRecord::Migration[5.0]
   def change
-    add_column :users, :role, :string
-
-    User.update_all(role: 'admin')
-
-    change_column :users, :role, :string, null: false
+    add_column :users, :role, :string, null: false
   end
 end
--- db/migrate/20170104084419_add_config_type.rb
+++ db/migrate/20170104084419_add_config_type.rb
@@ -1,18 +1,5 @@
 class AddConfigType < ActiveRecord::Migration[5.0]
   def change
     add_column :custom_configs, :config_type, :string
-
-    CustomConfig.all.each do |config|
-      config.config_type = CustomConfig::SETTINGS
-      if config.config.is_a?(Array)
-        puts "FROM: #{config.config.inspect}"
-        config.config = config.config.each_with_object({}) { |c, h| h[c['name']] = c['value'] }
-        puts "TO: #{config.config.inspect}"
-      end
-      config.save
-    end
-
-    Business.all.each { |b| CustomConfig.build_for(b).save }
-    Portfolio.all.each { |p| CustomConfig.build_for(p).save }
   end
 end
