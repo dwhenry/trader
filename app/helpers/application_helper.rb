be4a91a96ef24e345527f224e4f8e0942d22a7f3
David Henry <david@decoybecoy.com>
2017-01-29 12:10:54 +0000

    Tidy up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     { 'business' => 'Business', 'user' => 'User', 'portfolios' => "Portfolio's" }
   end
 
-  def configuration_options
+  def copy_portfolio_config_options
     [['Use default options', 0]] + Portfolio.where(business_id: current_user.business_id).pluck(:name, :id)
   end
 end
